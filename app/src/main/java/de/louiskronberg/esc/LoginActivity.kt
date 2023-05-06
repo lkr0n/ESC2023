45e37cc22c122eaf985b7462ff27d0999b58576b
M4tthewDE <mattider2@gmail.com>
2023-05-06 09:14:21 +0200

    Only ask for name once

--- app/src/main/java/de/louiskronberg/esc/LoginActivity.kt
+++ app/src/main/java/de/louiskronberg/esc/LoginActivity.kt
@@ -1,24 +1,12 @@
 package de.louiskronberg.esc
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
-import com.google.gson.Gson
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.headers
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
 
 
@@ -27,55 +15,34 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val signInButton: SignInButton = findViewById(R.id.sign_in_button)
-        signInButton.setSize(SignInButton.SIZE_STANDARD)
-
-        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            signInButton.visibility = View.INVISIBLE
-
-            val submitButton: Button = findViewById(R.id.submit_button)
-            val nameText: TextView = findViewById(R.id.name_text)
-            submitButton.visibility = View.VISIBLE
-            nameText.visibility = View.VISIBLE
-
-            submitButton.setOnClickListener {
-                // check if user exists!
-                runBlocking {
-                    createUser(
-                        nameText.text.toString(),
-                        GoogleSignIn.getLastSignedInAccount(applicationContext)!!.idToken!!
-                    )
-                }
+        val intent = Intent(this, UserActivity::class.java)
+        val userLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 finish()
             }
+
+        val googleLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val idToken =
+                GoogleSignIn.getLastSignedInAccount(applicationContext)!!.idToken!!
+
+            val user = runBlocking { UserApi.getUser(getString(R.string.api_url), idToken) }
+            if (user != null) {
+                finish()
+            } else {
+                userLauncher.launch(intent)
+            }
         }
+
+        val signInButton: SignInButton = findViewById(R.id.sign_in_button)
+        signInButton.setSize(SignInButton.SIZE_STANDARD)
 
         signInButton.setOnClickListener {
             val gso =
                 GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestIdToken(getString(R.string.client_id)).build()
             val googleSignInClient = GoogleSignIn.getClient(this, gso)
-            launcher.launch(googleSignInClient.signInIntent)
+            googleLauncher.launch(googleSignInClient.signInIntent)
         }
     }
 
-
-    data class UserBody(val name: String)
-
-    private suspend fun createUser(name: String, idToken: String) {
-        val url = getString(R.string.api_url) + "/user"
-        val body = Gson().toJson(UserBody(name))
-        val client = HttpClient(CIO)
-        val result = client.post(url) {
-            contentType(ContentType.Application.Json)
-            headers {
-                append("Id-Token", idToken)
-            }
-            setBody(body)
-        }
-        if (result.status != HttpStatusCode.OK) {
-            Log.i("ERROR", result.toString())
-            return
-        }
-    }
 }
---
+++ app/src/main/java/de/louiskronberg/esc/UserActivity.kt
@@ -0,0 +1,32 @@
+package de.louiskronberg.esc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import kotlinx.coroutines.runBlocking
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+
+        val submitButton: Button = findViewById(R.id.submit_button)
+        val nameText: TextView = findViewById(R.id.name_text)
+
+        submitButton.setOnClickListener {
+            val idToken =
+                GoogleSignIn.getLastSignedInAccount(applicationContext)!!.idToken!!
+            runBlocking {
+                UserApi.createUser(
+                    getString(R.string.api_url),
+                    nameText.text.toString(),
+                    idToken
+                )
+            }
+            finish()
+        }
+    }
+
+}
