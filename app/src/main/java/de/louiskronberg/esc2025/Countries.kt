d1ea7d4168e65e175d9db68c44ecaaeaee6220e6
M4tthewDE <matthias.kronberg@gmail.com>
2025-05-16 12:04:34 +0200

    update

--- app/src/main/java/de/louiskronberg/esc2025/Countries.kt
+++ app/src/main/java/de/louiskronberg/esc2025/Countries.kt
@@ -19,41 +19,161 @@ class Countries {
 
          val countries = arrayOf(
              Country(
-                 "Deutschland",
-                 R.drawable.germany,
-                 "Isaak",
-                 "Always on the Run"
+                 "Norwegen",
+                 R.drawable.norway,
+                 "Kyle Alessandro",
+                 "Lighter"
+             ),
+             Country(
+                 "Luxemburg",
+                 R.drawable.luxembourg,
+                 "Laura Thorn",
+                 "La poupée monte le son"
+             ),
+             Country(
+                 "Estland",
+                 R.drawable.estonia,
+                 "Tommy Cash",
+                 "Espresso Macchiato"
+             ),
+             Country(
+                 "Israel",
+                 R.drawable.israel,
+                 "Yuval Raphael",
+                 "New Day Will Rise"
+             ),
+             Country(
+                 "Litauen",
+                 R.drawable.lithuania,
+                 "Katarsis",
+                 "Tavo akys"
              ),
              Country(
                  "Spanien",
                  R.drawable.spain,
-                 "Nebulossa",
-                 "Zorra"
+                 "Melody",
+                 "Esa diva"
              ),
              Country(
-                 "Großbritannien",
+                 "Ukraine",
+                 R.drawable.ukraine,
+                 "Ziferblat",
+                 "Bird of Pray"
+             ),
+             Country(
+                 "Vereinigtes Königreich",
                  R.drawable.unitedkingdom,
-                 "Olly Alexander",
-                 "Dizzy"
+                 "Remember Monday",
+                 "What the Hell Just Happened?"
+             ),
+             Country(
+                 "Österreich",
+                 R.drawable.austria,
+                 "JJ",
+                 "Wasted Love"
+             ),
+             Country(
+                 "Island",
+                 R.drawable.iceland,
+                 "Væb",
+                 "Róa"
+             ),
+             Country(
+                 "Lettland",
+                 R.drawable.latvia,
+                 "Tautumeitas",
+                 "Bur man laimi"
+             ),
+             Country(
+                 "Niederlande",
+                 R.drawable.netherlands,
+                 "Claude",
+                 "C'est la vie"
+             ),
+             Country(
+                 "Finnland",
+                 R.drawable.finland,
+                 "Erika Vikman",
+                 "Ich komme"
              ),
              Country(
                  "Italien",
                  R.drawable.italy,
-                 "Angelina Mango",
-                 "La noia"
+                 "Lucio Corsi",
+                 "Volevo essere un duro"
+             ),
+             Country(
+                 "Polen",
+                 R.drawable.poland,
+                 "Justyna Steczkowska",
+                 "Gaja"
+             ),
+             Country(
+                 "Deutschland",
+                 R.drawable.germany,
+                 "Abor & Tynna",
+                 "Baller"
+             ),
+             Country(
+                 "Griechenland",
+                 R.drawable.greece,
+                 "Klavdia",
+                 "Asteromata"
+             ),
+             Country(
+                 "Armenien",
+                 R.drawable.armenia,
+                 "PARG",
+                 "Survivor"
              ),
              Country(
                  "Schweiz",
                  R.drawable.switzerland,
-                 "Nemo",
-                 "The Code"
+                 "Zoë Më",
+                 "Voyage"
+             ),
+             Country(
+                 "Malta",
+                 R.drawable.malta,
+                 "Miriana Conte",
+                 "Serving"
+             ),
+             Country(
+                 "Portugal",
+                 R.drawable.portugal,
+                 "NAPA",
+                 "Deslocado"
+             ),
+             Country(
+                 "Dänemark",
+                 R.drawable.denmark,
+                 "Sissal",
+                 "Hallucination"
+             ),
+             Country(
+                 "Schweden",
+                 R.drawable.sweden,
+                 "KAJ",
+                 "Bara bada bastu"
              ),
              Country(
                  "Frankreich",
                  R.drawable.france,
-                 "Slimane",
-                 "Mon amour"
+                 "Louane",
+                 "Maman"
              ),
+             Country(
+                 "San Marino",
+                 R.drawable.sanmarino,
+                 "Gabry Ponte",
+                 "Tutta l'Italia"
+             ),
+             Country(
+                 "Albanien",
+                 R.drawable.albania,
+                 "Shkodra Elektronike",
+                 "Zjerm"
+             )
          )
      }
 }
